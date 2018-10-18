cd1f1e7e4a3c1052986ed19fdb5de8652c3f8660
haid <hmdlohar@gmail.com>
2018-10-18 18:04:08 +0530

    merge

--- admin/header.php
+++ admin/header.php
@@ -22,7 +22,7 @@ require_once "admin_logged.php";
   <body>
 
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">pEdit Admin Panel</a>
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">iEdit Admin Panel</a>
       
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
